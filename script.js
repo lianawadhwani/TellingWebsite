7da55033b8a4e7c68d1f23ae122bc254ed8339bf
Liana Wadhwani <lianawadhwani31@gmail.com>
2022-07-25 15:47:44 -0700

    added text next to bullet point

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ var events= ["cough", "wheeze"];
 let divList= document.getElementById('listHolder');
 function message(){
   let word = document.getElementById("text").value;
+  console.log(word);
   let ul = document.getElementById('list');
   let li = document.createElement('li');
+  li.innerHTML = word;
   ul.appendChild(li);
   events.push(word);
 }
